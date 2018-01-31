3e0e40053979b7ca43758a31e4f7ab9f5d3e215a
Hannah Lee and Mariah Shipley <hannah@>
2018-01-31 10:21:53 -0800

    add audio file and functions.

---
+++ js/audio.js
@@ -0,0 +1,7 @@
+const ambientSound = new Audio("./audio/ambient.wav");
+
+const doorSound = new Audio("./audio/door.wav");
+
+const footstepsSound = new Audio("./audio/footsteps.wav");
+
+const wakeupSound = new Audio("./audio/wakeup.wav");
