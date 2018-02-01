5f5a50fcc7debec29463de09b239353878886d68
runhannah <leexhannah@gmail.com>
2018-02-01 12:03:44 -0800

    add background audio files

--- js/audio.js
+++ js/audio.js
@@ -9,16 +9,20 @@ const airSound = new Audio("./audio/air.wav");
 
 const alertSound = new Audio ("./audio/alert.wav");
 
-const alienRoarSound = new Audio("./audio/alienRoar.mp3")
+const alienRoarSound = new Audio("./audio/alienRoar.mp3");
 
-const beepingSound = new Audio("./audio/beeping.mp3")
+const beepingSound = new Audio("./audio/beeping.mp3");
 
-const buttonSound = new Audio("./audio/button.mp3")
+const buttonSound = new Audio("./audio/button.mp3");
 
-const rustleSound = new Audio("./audio/rustle.wav")
+const rustleSound = new Audio("./audio/rustle.wav");
 
-const rustleThreeSound = new Audio("./audio/rustleThree.wav")
+const rustleThreeSound = new Audio("./audio/rustleThree.wav");
 
-const slimeSound = new Audio("./audio/slime.wav")
+const slimeSound = new Audio("./audio/slime.wav");
 
-const wilhelmSound = new Audio("./audio/wilhelm.wav")
+const wilhelmSound = new Audio("./audio/wilhelm.wav");
+
+const backgroundOneSound = new Audio("./audio/backgroundOne.aiff");
+
+const backgroundTwoSound = new Audio("./audio/backgroundTwo.wav")
